7559e5f3dc27e4cbcb3f0d67e7e8be116719a16f
Степан Захаров <stepanzh@gmail.com>
2021-05-14 15:54:07 +0300

    [tenprint, config] Add config variables and support of pattern for Maze
    Config has new variables.
    
    tenprint.py
      Maze can be generated with all default arguments from config
      Maze support pattern instead of chars
      upd Maze _calc_neighbours function up corresponding to MazePattern

--- lib/tenprint.py
+++ lib/tenprint.py
@@ -1,64 +1,78 @@
 import random
 from collections import deque
 from lib.utils import Size
+from lib.mazepattern import MazePattern
+
 import lib.config as config
 
 DEFAULT_SEED = config.DEFAULT_MAZE_SEED
-DEFAULT_CHARS = config.DEFAULT_MAZE_CHARS
+DEFAULT_SIZE = config.DEFAULT_MAZE_SIZE
+DEFAULT_PATTERN = MazePattern()
 
 
 class Maze(object):
     size:Size = None
     maze = None
     _components = None
-    chars = DEFAULT_CHARS
     _vertex_belong = None
     _neighbours = None
 
-    def __init__(self, size, seed=DEFAULT_SEED, chars=DEFAULT_CHARS):
+    def __init__(self, size=DEFAULT_SIZE, seed=DEFAULT_SEED, pattern=DEFAULT_PATTERN):
         "Creates `Maze` object from `size = (width, height)`, `seed` for `random.seed`, and pair of `chars`."
 
         self.size = Size(width=size[0], height=size[1])
-        self.maze = Maze.generate(size, seed)
+        self.maze = Maze.generate(size, seed, pattern)
+        self._pattern = pattern
         
         self._calc_neighbours()     # self._neighbours
         self._calc_components()     # self._components
         self._calc_vertex_belong()  # self._vertex_belong
-        
-        self.chars = tuple(chars)
 
     @classmethod
-    def generate(cls, size, seed=DEFAULT_SEED, chars=DEFAULT_CHARS):
+    def generate(cls, size=DEFAULT_SIZE, seed=DEFAULT_SEED, pattern=DEFAULT_PATTERN):
         "Returns matrix of `size[0] x size[1]` with `chars`. Sets globally `seed`."
         random.seed(seed)
+        chars = pattern.chars()
         maze = tuple( tuple( chars[int(0.5 + random.random())] for _ in range(size[0]) ) for _ in range(size[1]))
         return maze
 
     def _calc_neighbours(self):
-        "Returns list of neighbours (i^, j^) vertex `v = (i, j)` for maze of chars '╱╲'."
+        """
+        self._neighbours:
+        Calculates list of neighbours (i^, j^) vertex `v = (i, j)` for maze of chars '╱╲'.
+        """
         
         self._neighbours = self.fill_matrix()
         
         maze = self.maze
         w, h = self.size.width, self.size.height
 
+        # ghost_maze is the same maze with additional empty-char boundaries on each side
+        ghost_maze = []
+        fill = self._pattern.fill()
+        ghost_maze.append([fill] * (self.size.width + 2))  # top
+        for i in range(h):
+            ghost_maze.append(
+                [fill] + list(self.maze[i][:]) + [fill]    # inner
+            )
+        ghost_maze.append([fill] * (self.size.width + 2))  # bottom
+
+        # cycle goes over positions in original maze
         for i in range(h):
             for j in range(w):
+                # i_ghost = i + 1, j_ghost = j + 1
+                # need to create window from (i-1, j-1) up to (i+1, j+1)
+                window = [
+                    ghost_maze[i][j:j+3],    # row above: [i-1] -> [i-1+1] = [i]
+                    ghost_maze[i+1][j:j+3],  # [j-1:j+2] -> [j-1+1:j+2+1] = [j:j+3]
+                    ghost_maze[i+2][j:j+3]
+                ]
+                adj = self._pattern.adjacent(window, is_matrix=True)
                 neigh = set()
-                for di, dj in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+                for di, dj in adj:
                     ii, jj = i + di, j + dj
-                    if 0 <= ii < h and 0 <= jj < w and maze[ii][jj] != maze[i][j]:
+                    if 0 <= ii < h and 0 <= jj < w:
                         neigh.add((ii, jj))
-                if maze[i][j] == self.chars[0]:  # /
-                    for di, dj in ((-1, 1),  (1, -1)):  # di + dj == 0
-                        ii, jj = i + di, j + dj
-                        if 0 <= ii < h and 0 <= jj < w and maze[ii][jj] == maze[i][j]:
-                            neigh.add((ii, jj))
-                if maze[i][j] == self.chars[1]:  # \
-                    for di, dj in ((-1, -1), (1, 1)):
-                        ii, jj = i + di, j + dj
-                        if 0 <= ii < h and 0 <= jj < w and maze[ii][jj] == maze[i][j]:
-                            neigh.add((ii, jj))
                 self._neighbours[i][j] = frozenset(neigh)
 
     def neighbours(self, v):
